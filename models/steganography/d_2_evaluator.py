bea68cdfa756ed915428edef175a7f37796b564f
Timothy Henry <timhenry@mit.edu>
2018-05-09 17:25:00 -0400

    Added raw video and their 32x32 frames, with code used

--- models/steganography/d_2_evaluator.py
+++ models/steganography/d_2_evaluator.py
@@ -83,24 +83,29 @@ class SteganographyImageCoverWrapper:
         print(list(secret.flatten().astype(int)))
         return string_from_bits(list(secret.flatten().astype(int)))
 
-def vid_to_frames(location):
-    cap = cv2.VideoCapture(location)
+def video_to_frames(directory, filename, frame_size=32):
+    print("Converting video " + filename + " to frames")
+
+    cap = cv2.VideoCapture(directory + filename)
     fourcc = VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('./data/vid/small_vid.mp4', fourcc, float(20), (32, 32), True)
-    out2 = cv2.VideoWriter('./data/vid/center_small_vid.mp4', fourcc, float(20), (32, 32), True)
+
+    center_location = './data/centered_video_frames/' + filename[:-4] + '/'
+    scaled_location = './data/scaled_video_frames/' + filename[:-4] + '/'
 
     try:
-        if not os.path.exists('data'):
-            os.makedirs('data')
+        if not os.path.exists(center_location):
+            os.makedirs(center_location)
     except OSError:
-        print ('Error: Creating directory of data')
+        print ('Error: Creating directory of data ' + center_location)
+
+    try:
+        if not os.path.exists(scaled_location):
+            os.makedirs(scaled_location)
+    except OSError:
+        print ('Error: Creating directory of data ' + scaled_location)
 
     currentFrame = 0
     while(True):
-        if not(currentFrame % 15 == 0):
-            pass
-           # continue
-
         # Capture frame-by-frame
         ret, frame = cap.read()
 
@@ -109,16 +114,12 @@ def vid_to_frames(location):
 
         num = '{0:04}'.format(currentFrame)
 
-        # Saves image of the current frame in jpg file
-        name = './data/vid/png/frame' + num + '.png'
-        name2 = './data/vid/pngs/frame' + num + '.png'
-        #print(frame.shape)
-        #print ('Creating...' + name)
-        cv2.imwrite(name2, frame[344:376,624:656])
-        out2.write(frame[344:376, 624:656])
-        #print(frame[:704:22, ::40].shape)
-        cv2.imwrite(name, frame[:704:22, ::40])
-        out.write(frame[:704:22, ::40])
+        # Saves image of the current frame in png file
+        centered_name = center_location + num + '.png'
+        scaled_name = scaled_location + num + '.png'
+        x, y, _ = frame.shape
+        cv2.imwrite(centered_name, frame[int((x/2) - (frame_size/2)) : int((x/2) + (frame_size/2)), int((y/2) - (frame_size/2)): int((y/2) + (frame_size/2))])
+        cv2.imwrite(scaled_name, frame[:x - (x % frame_size) : x // frame_size, :y - (y % frame_size): y // frame_size])
 
         # To stop duplicate images
         currentFrame += 1
@@ -127,24 +128,35 @@ def vid_to_frames(location):
     cap.release()
     cv2.destroyAllWindows()
 
-def video_in_video(helper):
-    #vid_to_frames('data/vid/vid.mp4')
+    return center_location, scaled_location
 
-    image_dir = 'data/vid/png'
-    covers = load_images(path=image_dir + 's', scale=model.image_scale)#,
-    secrets = load_images(path=image_dir, scale=model.image_scale)
+def video_in_video(helper, secret_path, cover_path, request_number):
+    covers = load_images(path=cover_path, scale=model.image_scale)
+    secrets = load_images(path=secret_path, scale=model.image_scale)
+
+    output_dir = "./data/video_output/" + request_number + "/"
     fourcc = VideoWriter_fourcc(*'XVID')
-    #hidden_vid = cv2.VideoWriter('./data/vid/small_vid_covered.mp4', fourcc, float(30), (32, 32), True)
-    #revealed_vid = cv2.VideoWriter('./data/vid/small_vid_revealed.mp4', fourcc, float(30), (32, 32), True)
-    combined_vid = cv2.VideoWriter('./data/vid/combined_vid.mp4', fourcc, float(30), (64, 64), True)
+    cover_vid = cv2.VideoWriter(output_dir + 'cover.mp4', fourcc, float(30), (32, 32), True)
+    secret_vid = cv2.VideoWriter(output_dir + 'secret.mp4', fourcc, float(30), (32, 32), True)
+    hidden_vid = cv2.VideoWriter(output_dir + 'hidden.mp4', fourcc, float(30), (32, 32), True)
+    revealed_vid = cv2.VideoWriter(output_dir + 'revealed.mp4', fourcc, float(30), (32, 32), True)
+    combined_vid = cv2.VideoWriter(output_dir + 'combined.mp4', fourcc, float(30), (64, 64), True)
 
+    # Iterate through all frames
     for i in range(len(secrets)):
-        cover, secret = covers[i], secrets[i]
+        # Produce the 4 frames
+        cover, secret = covers[i % len(covers)], secrets[i]
         hidden_secret = np.array(array_to_img(helper.hide_given_image_in_given_cover(cover, secret)))
         revealed_secret = np.array(array_to_img(helper.decode_image_in_cover(hidden_secret)))
         cover, secret = np.array(array_to_img(cover)), np.array(array_to_img(secret))
-        #hidden_vid.write(hidden_secret)
-        #revealed_vid.write(revealed_secret)
+
+        # Write individual frames
+        cover_vid.write(cover)
+        secret_vid.write(secret)
+        hidden_vid.write(hidden_secret)
+        revealed_vid.write(revealed_secret)
+
+        # Combine individual frames
         frameTop = np.concatenate((np.array(array_to_img(secret)), np.array(array_to_img(cover))), axis=1)
         frameBottom = np.concatenate((hidden_secret, revealed_secret), axis=1)
         frame = np.concatenate((frameTop, frameBottom), axis=0)
@@ -168,7 +180,15 @@ if __name__ == '__main__':
 
     helper = SteganographyImageCoverWrapper(model)
 
-    video_in_video(helper)
+    path = './data/videos/'
+    for file in os.listdir(path):
+        _ , _ = video_to_frames(path, file, frame_size=32)
+    #video_in_video(helper)
+
+
+
+
+
 
 
 
